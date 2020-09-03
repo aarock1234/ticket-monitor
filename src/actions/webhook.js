7fd40e0ceae397a4bcb855c834e70ca840c98338
Rock <arockplaysmc@gmail.com>
2020-09-03 00:52:09 -0700

    Reduced size of webhook.js

--- src/actions/webhook.js
+++ src/actions/webhook.js
@@ -6,80 +6,38 @@ const {
     sleep
 } = require('../utils/tools.js');
 
-let embedTemplate = {
-    embeds: [{
-        color: null,
-        title: null,
-        author: {
-            name: 'Made with <3 by Rock @ StormeIO',
-            icon_url: 'https://i.imgur.com/e5vzoUb.png'
-        },
-        fields: [
-            {
-                name: 'Site',
-                value: 'Supreme',
-            },
-            {
-                name: 'Script URL',
-                value: null,
-            },
-            {
-                name: 'Script SHA256 Checksum',
-                value: null,
-            },
-        ],
-        timestamp: new Date(),
-        footer: {
-            text: 'Made with <3 by Rock @ StormeIO',
-            icon_url: 'https://i.imgur.com/e5vzoUb.png',
-        },
-    }]
-}
-
 module.exports = {
-    added: async newScript => {
-        try {
-            embedTemplate.color = 1305395;
-            embedTemplate.title = 'New Script Added'
-            embedTemplate.embeds[0].fields[1].value = newScript.url;
-            embedTemplate.embeds[0].fields[1].value = newScript.shaChecksum;
-    
-            module.exports.send();
-        } catch (e) {
-            console.error(`ERR (WH): ${e.message}`);
-            await sleep(config.delay);
-            return module.exports.added(newScript);
+    send: async (color, title, script) => {
+        const embed = {
+            embeds: [{
+                color: color,
+                title: title,
+                author: {
+                    name: 'Made with <3 by Rock @ StormeIO',
+                    icon_url: 'https://i.imgur.com/e5vzoUb.png'
+                },
+                fields: [
+                    {
+                        name: 'Site',
+                        value: 'Supreme',
+                    },
+                    {
+                        name: 'Script URL',
+                        value: script.url,
+                    },
+                    {
+                        name: 'Script SHA256 Checksum',
+                        value: script.shaChecksum,
+                    },
+                ],
+                timestamp: new Date(),
+                footer: {
+                    text: 'Made with <3 by Rock @ StormeIO',
+                    icon_url: 'https://i.imgur.com/e5vzoUb.png',
+                },
+            }]
         }
-    },
-    removed: async removedScript => {
-        try {
-            embedTemplate.color = 16722217;
-            embedTemplate.title = 'Script Removed'
-            embedTemplate.embeds[0].fields[1].value = removedScript.url;
-            embedTemplate.embeds[0].fields[1].value = removedScript.shaChecksum;
 
-            module.exports.send();
-        } catch (e) {
-            console.error(`ERR (WH): ${e.message}`);
-            await sleep(config.delay);
-            return module.exports.removed(removedScript);
-        }
-    },
-    edited: async editedScript => {
-        try {
-            embedTemplate.color = 16769090;
-            embedTemplate.title = 'Script Contents Edited'
-            embedTemplate.embeds[0].fields[1].value = removedScript.url;
-            embedTemplate.embeds[0].fields[1].value = removedScript.shaChecksum;
-            
-            module.exports.send();
-        } catch (e) {
-            console.error(`ERR (WH): ${e.message}`);
-            await sleep(config.delay);
-            return module.exports.removed(editedScript);
-        }
-    },
-    send: async () => {
         try {
             await request({
                 url: config.webhook,
@@ -87,7 +45,7 @@ module.exports = {
                 headers: {
                     'content-type': 'application/json',
                 },
-                body: JSON.stringify(embedTemplate)
+                body: JSON.stringify(embed)
             })
         } catch (e) {
             console.error(`ERR (WHS): ${e.message}`);
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ const webhookManager = require('./actions/webhook.js');
 let currentMonitor = new scriptMonitor();
 
 currentMonitor.on('added', script => {
-    webhookManager.added(script);
+    webhookManager.send(1305395, 'New Script Added', script);
     console.log('New Script')
 });
 
 currentMonitor.on('removed', script => {
-    webhookManager.removed(script);
+    webhookManager.send(16722217, 'Script Removed', script);
     console.log('Script Removed')
 });
 
 currentMonitor.on('edited', script => {
-    webhookManager.edited(script);
+    webhookManager.send(16769090, 'Script Contents Edited', script);
     console.log('Script Edited');
 });
