6f222f707a05486f58ac57d8ab62638b4c30c965
Rock <arockplaysmc@gmail.com>
2020-09-02 23:45:12 -0700

    Removed debug code in index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 const scriptMonitor = require('./monitor.js');
 const webhookManager = require('./actions/webhook.js');
 
-return webhookManager.added({
-    url: 'https://www.example.com',
-    shaChecksum: '4128d163becca8bccabd6a9f8fe8e603e630432f133fe67fd779cc64534bca27'
-});
-
 let currentMonitor = new scriptMonitor();
 
 currentMonitor.on('added', script => webhookManager.added(script));
